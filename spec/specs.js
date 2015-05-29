e6cb9be320852c78a4f8dded677ec74782954fe4
James Williams <jamesrwilliams91@gmail.com>
2015-05-29 09:37:29 -0700

    wrote and passed second spec, will change multiple instances of one o=word to another word

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,11 @@
 describe('findAndReplace', function() {
+
   it("Will find one word and replace it with another word in a phrase", function() {
     expect(findAndReplace("the dog is good", "dog", "cat")).to.equal("the cat is good")
+  });
+
+  it("Will find multiple instances of one words and replace it with another word in a phrase", function() {
+    expect(findAndReplace("the dog is good to the dog", "dog", "cat")).to.equal("the cat is good to the cat")
   });
 
 });
