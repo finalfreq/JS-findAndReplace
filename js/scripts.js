8dab99c2154eb49a8011340c0ff2e182b2f37896
James Williams <jamesrwilliams91@gmail.com>
2015-05-29 11:18:30 -0700

    took out debugger code from JS file

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 var findAndReplace = function(phrase, wordToReplace, replacementWord) {
   var words = phrase.toLowerCase().replace(/\W+/g, " ").split(' ')
+
   for (var i = 0; i < words.length; i++) {
     if (words[i] === wordToReplace) {
       words[i] = replacementWord;
     }
   }
+
   return words.join(" ")
 };
 
 $(document).ready(function() {
   $("form#calculator").submit(function(event) {
-    $("#result").show();
     var phrase = $("input#phrase").val();
     var word = $("input#word").val();
     var replacement = $("input#replacementword").val();
     var newPhrase = findAndReplace(phrase, word, replacement)
-    debugger;
+
+    $("#result").show();
     $(".result").text(newPhrase);
 
     event.preventDefault();
